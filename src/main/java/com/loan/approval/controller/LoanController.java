bf6349d64626a6951983e5c3307f0a238c6c47ac
Prakash <omprakash4@deloitte.com>
2025-08-15 16:31:44 +0530

    milestone 2 done

--- src/main/java/com/loan/approval/controller/LoanController.java
+++ src/main/java/com/loan/approval/controller/LoanController.java
@@ -1,5 +1,6 @@
 package com.loan.approval.controller;
 
+import com.loan.approval.model.LoanRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,5 +13,12 @@ public class LoanController {
     public ResponseEntity<?> createNewLoanRequest(@RequestParam String userName){
         System.out.println("User Name is : -> " + userName);
         return new ResponseEntity<>("Created", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/send-email")
+    public ResponseEntity<String> sendEmailForApproval(@RequestBody LoanRequest loanRequest){
+        System.out.println("Received for approval status " + loanRequest.getEligibleForLoan());
+        System.out.println("Received user name : " + loanRequest.getUser().getUserName());
+        return new ResponseEntity<>("Received for approval",HttpStatus.OK);
     }
 }
--- src/main/java/com/loan/approval/model/LoanRequest.java
+++ src/main/java/com/loan/approval/model/LoanRequest.java
@@ -1,8 +1,34 @@
 package com.loan.approval.model;
 
 import jakarta.persistence.Entity;
-
+import org.springframework.stereotype.Component;
 
 public class LoanRequest {
+    private User user;
+    private Boolean isEligibleForLoan;
+    private Integer loanAmount;
 
+    public Integer getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(Integer loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public Boolean getEligibleForLoan() {
+        return isEligibleForLoan;
+    }
+
+    public void setEligibleForLoan(Boolean eligibleForLoan) {
+        isEligibleForLoan = eligibleForLoan;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
